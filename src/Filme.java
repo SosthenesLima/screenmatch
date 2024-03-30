90ee3a3d7185bdca2b846646f8167de37545e0b1
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-03-30 10:25:49 -0300

    Commit-Definindo ações para o filme01

--- src/Filme.java
+++ src/Filme.java
@@ -2,7 +2,19 @@ public class Filme {
     String nome;
     int anoDeLancamento;
     boolean incluidoNoPlano;
-    double avalicacao;
+    double somaDasAvaliacoes;
     int totalDeAvalicacoes;
     int duracaoEmMinutos;
+
+    void exibeFichaTecnica(){
+        System.out.println("nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+    }
+
+    void avalia(double nota) {
+        somaDasAvaliacoes += nota;
+        somaDasAvaliacoes++;
+
+
+    }
 }

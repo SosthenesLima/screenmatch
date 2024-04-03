a83ff76fbcec558d35964b3f2429e77b26240769
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-04-02 21:38:34 -0300

    Commit -Criando métodos que modificam valores01

--- src/Principal.java
+++ src/Principal.java
@@ -1,13 +1,13 @@
+import br.com.lima.screenmatch.modelos.Filme;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "O Poderoso Chefão";
-        meuFilme.anoDeLancamento = 1970;
-        meuFilme.duracaoEmMinutos = 180;
-
-
+        meuFilme.setNome( "O Poderoso Chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
 
 
         meuFilme.exibeFichaTecnica();
---
+++ src/br/com/lima/screenmatch/modelos/Filme.java
@@ -0,0 +1,63 @@
+package br.com.lima.screenmatch.modelos;
+
+public class Filme {
+   private String nome;
+    private int anoDeLancamento;
+   private boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalDeAvalicacoes;
+    private int duracaoEmMinutos;
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    public int getTotalDeAvalicacoes(){
+        return totalDeAvalicacoes;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+
+
+    public void exibeFichaTecnica(){
+        System.out.println("nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+    }
+
+    public void avalia(double nota) {
+        somaDasAvaliacoes += nota;
+        totalDeAvalicacoes++;
+    }
+
+    public double pegaMedia(){
+        return somaDasAvaliacoes / totalDeAvalicacoes;
+    }
+}

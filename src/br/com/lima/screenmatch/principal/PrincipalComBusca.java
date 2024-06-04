bcfad3d55af5b15ed949ec27c4131d677a4eabf1
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-06-03 21:38:52 -0300

    Commit - Implementando a comunicação com o Web Service04

--- src/br/com/lima/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/lima/screenmatch/principal/PrincipalComBusca.java
@@ -1,14 +1,18 @@
 package br.com.lima.screenmatch.principal;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class PrincipalComBusca {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://foo.com/"))
+                .uri(URI.create("https://www.omdbapi.com/?t=Matrix&apikey=e4c92d6d"))
                 .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
     }
 }

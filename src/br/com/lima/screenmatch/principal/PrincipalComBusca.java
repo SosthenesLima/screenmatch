d3a7abf5fa3d6c40ea9b8cdd324116206d378124
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-06-04 19:23:56 -0300

    Commit - Implementando a comunicação com o Web Service05

--- src/br/com/lima/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/lima/screenmatch/principal/PrincipalComBusca.java
@@ -14,5 +14,6 @@ public class PrincipalComBusca {
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
     }
 }

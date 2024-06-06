6bfe4815f25df22498c9b0648729ff79cb7fd428
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-06-06 20:21:26 -0300

    Commit - Refinando a busca por filmes01

--- src/br/com/lima/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/lima/screenmatch/principal/PrincipalComBusca.java
@@ -9,9 +9,16 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
+
+        Scanner leitura =  new Scanner(System.in);
+        System.out.println("Digite um filme para busca: ");
+
+        var busca = leitura.nextLine();
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("https://www.omdbapi.com/?t=Matrix&apikey=e4c92d6d"))

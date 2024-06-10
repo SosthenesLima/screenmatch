1efefa723b74bd4a3d2fa222fde5e8495e9b4b42
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-06-09 21:40:12 -0300

    Commit - Transformando dados02

--- src/br/com/lima/screenmatch/modelos/Titulo.java
+++ src/br/com/lima/screenmatch/modelos/Titulo.java
@@ -4,9 +4,13 @@
 
 package br.com.lima.screenmatch.modelos;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Titulo implements Comparable<Titulo>{
 
+    @SerializedName("Title")
     private String nome;
+    @SerializedName("Year")
     private int anoDeLancamento;
     private boolean incluidoNoPlano;
     private double somaDasAvaliacoes;
@@ -74,5 +78,11 @@ public class Titulo implements Comparable<Titulo>{
     @Override
     public int compareTo(Titulo outroTitulo) {
         return this.getNome().compareTo(outroTitulo.getNome());
+    }
+
+    @Override
+    public String toString() {
+        return "nome='" + nome + '\'' +
+                ", anoDeLancamento=" + anoDeLancamento;
     }
 }
